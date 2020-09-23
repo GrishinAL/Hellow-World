3ee992c7bc241eccc180dc6aebac7d70ea2005a1
AlexGrishin <pipilipipipiu@gmail.com>
2020-09-23 20:53:07 +1000

    changed homework.py (comments)

--- test.py
+++ test.py
@@ -3,14 +3,6 @@
 a = [3, 2, 6, 2, 8, 1, 0, 1, 6, 0]
 b = [1, 0, 1, 0, 0]
 
-
-for i in range(0, 6):
-    if a[i] == b[0]:
-        for j in range(1, 5):
-            if a[i+1] == b[j]:
-                print('true')
-                i=i+1
-            else:
-                print('false')
-    else:
-        print('false')
+print (a)
+a.clear()
+print(a)
